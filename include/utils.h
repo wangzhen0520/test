32b0acf083223f9ec855bf55028d75ce8d5aaee2
wangzhen0520 <62492817+wangzhen0520@users.noreply.github.com>
2022-10-04 16:13:33 +0800

    Update utils.h
    add timecost module

--- include/utils.h
+++ include/utils.h
@@ -1,8 +1,11 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <sys/time.h>
 #include <mutex>
 #include <memory>
+#include <string>
+#include <iostream>
 
 template <typename T>
 class Singleton {
@@ -41,4 +44,57 @@ std::mutex Singleton<T>::instanceMutex_;
     friend class std::shared_ptr<type>; \
     friend class Singleton<type>;
 
+class TimeCost {
+public:
+    explicit TimeCost(const std::string &funcName)
+        : m_funcName(funcName), m_start{}, m_end{}, m_operStart{}, m_operEnd{}, m_interval(0)
+    {
+        gettimeofday(&m_start, nullptr);
+        m_operStart = m_start;
+    }
+
+    TimeCost(const std::string &funcName, uint32_t interval)
+        : m_funcName(funcName), m_start{}, m_end{}, m_operStart{}, m_operEnd{}, m_interval(interval)
+    {
+        gettimeofday(&m_start, nullptr);
+        m_operStart = m_start;
+    }
+
+    ~TimeCost()
+    {
+        gettimeofday(&m_end, nullptr);
+
+        int64_t cost = (int64_t)((m_end.tv_sec - m_start.tv_sec) * 1000) + (m_end.tv_usec - m_start.tv_usec) / 1000;
+
+        if (cost > m_interval) {
+            std::cout << m_funcName << " cost:" << cost << " ms" << std::endl;
+        }
+    }
+
+    /*
+     * 打印单个操作的耗时
+     * @param operatorName 操作名称
+     */
+    void OperatorCost(const std::string &operatorName)
+    {
+        gettimeofday(&m_operEnd, nullptr);
+
+        int64_t cost = (int64_t)((m_operEnd.tv_sec - m_operStart.tv_sec) * 1000) +
+                       (m_operEnd.tv_usec - m_operStart.tv_usec) / 1000;
+
+        if (cost > m_interval) {
+            std::cout << m_funcName << " " << operatorName << " cost:" << cost << " ms" << std::endl;
+        }
+
+        m_operStart = m_operEnd;
+    }
+
+private:
+    std::string m_funcName;
+    struct timeval m_start;
+    struct timeval m_end;
+    struct timeval m_operStart;
+    struct timeval m_operEnd;
+    uint32_t m_interval;  // 超时打印
+};
 #endif  // UTILS_H
